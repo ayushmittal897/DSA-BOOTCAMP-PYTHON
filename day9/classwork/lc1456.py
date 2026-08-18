3ddfadd2d5b5e5653e99e48c7c01c7a01a8a1676
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:33:43 +0530

    Update lc1456.py

--- day9/classwork/lc1456.py
+++ day9/classwork/lc1456.py
@@ -1,1 +1,27 @@
-# ddsjdnsj
+class Solution:
+    def maxVowels(self, s, k):
+        vowels = set("aeiou")
+        
+        count = 0
+        
+        # First window
+        for i in range(k):
+            if s[i] in vowels:
+                count += 1
+        
+        max_count = count
+        
+        # Slide the window
+        for i in range(k, len(s)):
+            if s[i] in vowels:
+                count += 1
+            
+            if s[i - k] in vowels:
+                count -= 1
+            
+            max_count = max(max_count, count)
+            
+            if max_count == k:
+                return k
+        
+        return max_count
