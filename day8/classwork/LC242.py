fac6a0cd53f1e7d9ce0925b88465544c4d9931c7
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:16:31 +0530

    Update LC242.py

--- day8/classwork/LC242.py
+++ day8/classwork/LC242.py
@@ -1,8 +1,20 @@
-# LC242
-# Problem: https://leetcode.com/problems/LC242/
-
-
 class Solution:
-    def solve(self):
-        pass
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
 
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count:
+                return False
+
+            count[char] -= 1
+
+            if count[char] < 0:
+                return False
+
+        return True
