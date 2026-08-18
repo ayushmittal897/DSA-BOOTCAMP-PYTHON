c12347217f93584f5107f48e4dac00ea4741d34b
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:19:01 +0530

    Implement isIsomorphic method in Solution class

--- day8/homework/LC205.py
+++ day8/homework/LC205.py
@@ -1,8 +1,16 @@
-# LC205
-# Problem: https://leetcode.com/problems/LC205/
-
-
 class Solution:
-    def solve(self):
-        pass
+    def isIsomorphic(self, s, t):
+        s_to_t = {}
+        t_to_s = {}
 
+        for a, b in zip(s, t):
+            if a in s_to_t and s_to_t[a] != b:
+                return False
+
+            if b in t_to_s and t_to_s[b] != a:
+                return False
+
+            s_to_t[a] = b
+            t_to_s[b] = a
+
+        return True
