e02ef695d28948e7458d9b7fc2092313847675ef
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:18:33 +0530

    Implement subarray sum solution in LC560.py

--- day8/classwork/LC560.py
+++ day8/classwork/LC560.py
@@ -1,8 +1,15 @@
-# LC560
-# Problem: https://leetcode.com/problems/LC560/
-
-
 class Solution:
-    def solve(self):
-        pass
+    def subarraySum(self, nums, k):
+        count = {0: 1}
+        prefix_sum = 0
+        result = 0
 
+        for num in nums:
+            prefix_sum += num
+
+            if prefix_sum - k in count:
+                result += count[prefix_sum - k]
+
+            count[prefix_sum] = count.get(prefix_sum, 0) + 1
+
+        return result
