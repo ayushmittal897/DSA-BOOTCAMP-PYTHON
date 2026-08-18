daff887d068a192f2fe6955ba65614e115fe9a3c
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:19:57 +0530

    Add topKFrequent method to Solution class
    Implemented topKFrequent method to find the k most frequent elements in an array.

--- day8/homework/LC347.py
+++ day8/homework/LC347.py
@@ -1,8 +1,23 @@
-# LC347
-# Problem: https://leetcode.com/problems/LC347/
-
-
 class Solution:
-    def solve(self):
-        pass
+    def topKFrequent(self, nums, k):
+        count = {}
 
+        # Count frequency
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        # Buckets: index = frequency
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in count.items():
+            buckets[freq].append(num)
+
+        result = []
+
+        # Start from highest frequency
+        for freq in range(len(buckets) - 1, 0, -1):
+            for num in buckets[freq]:
+                result.append(num)
+
+                if len(result) == k:
+                    return result
