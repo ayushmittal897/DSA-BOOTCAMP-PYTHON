6ce777de3880e394e5a325c1e12b1a905827b579
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-08-18 09:20:13 +0530

    Update LC454.py

--- day8/homework/LC454.py
+++ day8/homework/LC454.py
@@ -1,8 +1,21 @@
-# LC454
-# Problem: https://leetcode.com/problems/LC454/
-
-
 class Solution:
-    def solve(self):
-        pass
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        count = {}
 
+        # Store sums of nums1 + nums2
+        for a in nums1:
+            for b in nums2:
+                total = a + b
+                count[total] = count.get(total, 0) + 1
+
+        result = 0
+
+        # Find sums of nums3 + nums4 that cancel them
+        for c in nums3:
+            for d in nums4:
+                target = -(c + d)
+
+                if target in count:
+                    result += count[target]
+
+        return result
