c7ad75d17af1bb46fd1cdb67bfd64129bd5292d6
AYUSH MITTAL <86656981+ayushmittal897@users.noreply.github.com>
2026-09-01 15:47:14 +0530

    Update lc144.py

--- day12/Classwork/lc144.py
+++ day12/Classwork/lc144.py
@@ -1,1 +1,15 @@
 # lc144.py
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def dfs(node):
+            if node is None:
+                return
+
+            ans.append(node.val)   # Root
+            dfs(node.left)         # Left
+            dfs(node.right)        # Right
+
+        dfs(root)
+        return ans
